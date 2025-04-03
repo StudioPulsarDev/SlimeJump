4ef64ab653a1be3c98a9a90e4d29d741d557afb5
StudioPulsarDev <studiopulsardev@gmail.com>
2025-04-03 17:05:25 -0500

    c

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1743717637|791695040';
+const CACHE_VERSION = '1743717915|1070233488';
 /** @type {string} */
 const CACHE_PREFIX = 'Homepage Game-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
